30d42836be3a760b5b87d121306f907bf7be78eb
Zidras <10605951+Zidras@users.noreply.github.com>
2022-05-05 23:51:16 +0100

    Initial commit

---
+++ AllCheatersMustDie/AllCheatersMustDie.lua
@@ -0,0 +1,70 @@
+local date, GetTime = date, GetTime
+local print = print
+local tinsert, tremove, tContains = table.insert, table.remove, tContains
+local AddIgnore, IsIgnored = AddIgnore, IsIgnored
+local UnitName = UnitName
+
+-- Initialize Database
+if not AllCheatersMustDieDB then
+	AllCheatersMustDieDB = {
+		cheaters = {}
+	}
+end
+
+local msgDB, tempCheaters = {}, {}
+local function antiCheat(_, _, prefix, message, channel, sender)
+--	print("|cff00ff00[CheatersMustDie]|r "..sender.." "..message)
+	if UnitName("player") == sender then return end
+	if IsIgnored(sender) then return end
+	if tContains(tempCheaters, sender) then return end
+
+	if sender then
+		if not msgDB[sender] then
+			msgDB[sender] = {
+				channel = channel,
+				count = 0,
+				date = date(),
+				message = message,
+				prefix = prefix,
+				spamCount = 0,
+				timestamps = {},
+			}
+		end
+
+		-- update sender table and raise event counter
+		msgDB[sender].channel = channel
+		msgDB[sender].count = msgDB[sender].count + 1
+		msgDB[sender].date = date()
+		msgDB[sender].message = message
+		msgDB[sender].prefix = prefix
+		tinsert(msgDB[sender].timestamps, GetTime())
+
+		if #msgDB[sender].timestamps > 25 then
+			tremove(msgDB[sender].timestamps, 1)
+		end
+
+		-- compare timestamps
+		local tableIndex = #msgDB[sender].timestamps or 1
+		if tableIndex > 1 then
+			if msgDB[sender].timestamps[tableIndex] - msgDB[sender].timestamps[tableIndex - 1] < 0.1 then
+				msgDB[sender].spamCount = msgDB[sender].count + 1
+			else
+				msgDB[sender].spamCount = 0
+			end
+		end
+
+		-- check for cheater
+		-- if event counter is greater than 50, add to cheaters list
+		if msgDB[sender].spamCount > 50 then
+			AllCheatersMustDieDB.cheaters[sender] = msgDB[sender]
+			tinsert(tempCheaters, sender) -- needed because IsIgnored API was not returning in real time
+			AddIgnore(sender)
+			print("|cff00ff00[CheatersMustDie] |rDetected and ignored potential DoS attacker: ", sender,", with the following prefix: ", prefix, ", and message: ", message)
+		end
+	end
+end
+
+-- Log CHAT_MSG_ADDON messages
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:SetScript("OnEvent", antiCheat)
---
+++ README.md
@@ -0,0 +1,31 @@
+<div align="center">
+
+# All Cheaters Must Die (3.3.5a)
+
+[![Game Version](https://img.shields.io/badge/wow-3.3.5-blue.svg)](https://github.com/Zidras/AllCheatersMustDie)
+[![GitHub Actions](https://github.com/Zidras/AllCheatersMustDie/workflows/lint/badge.svg?branch=main&event=push)](https://github.com/Zidras/AllCheatersMustDie/actions?query=workflow%3Alint+branch%3Amain)
+[![Twitch](https://img.shields.io/twitch/status/the_zidras?style=social)](https://www.twitch.tv/the_zidras)
+[![Discord](https://img.shields.io/discord/598993375479463946.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/CyVWDWS)
+[![PayPal](https://img.shields.io/endpoint?url=https://www.stormfx.com/img/svg/paypal.json)](https://paypal.me/zidras)
+
+
+</div>
+
+**All Cheaters Must Die** is an addon created to serve as a defense against Denial-of-Service attacks by auto-ignoring the perpetrator before server disconnects.
+It also stores the information regarding the perpetrator in the SavedVariables folder, if needed.
+
+## How to use
+Only needs to be enabled in the AddOns list. Once you login, the addon will work automatically in the background.
+
+## Support
+If you would like to show your appreciation for my work (**which is by no means required**), you can donate in two ways:
+- [**Streamelements**](https://streamelements.com/the_zidras/tip): your Twitch name will show on my stream at the time of the donation, and in the widget as the latest donation!
+- [**PayPal**](https://paypal.me/zidras).
+
+## How to install and update the addon
+1. Download the addon from the **main** repository (https://github.com/Zidras/AllCheatersMustDie/archive/refs/heads/main.zip).
+2. Inside the zip file, open AllCheatersMustDie-main and copy (Ctrl+C) **AllCheatersMustDie** folder over to your addons folder (Interface/Addons).
+
+## Problems
+* If you've discovered something that's clearly wrong, or if you get an error, please create a [ticket](https://github.com/Zidras/AllCheatersMustDie/issues).
+* Feel free to join our [Discord](https://discord.gg/CyVWDWS) to talk, get help and discuss anything related to the addon!
